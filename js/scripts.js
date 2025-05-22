478032d93fc606e10e427af6b2061b9d52cc1c67
jose villegas <josevillegas13600@gmail.com>
2025-05-21 23:20:45 -0400

    createelement

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,23 @@ const enlaces = document.querySelectorAll('.navegacion a');
 console.log(enlaces[0]);
 enlaces[0].textContent = 'Nuevo texto para enlace';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+//enlaces[0].classList.remove('navegacion__enlace');
 //getelementbyid
 const heading2 = document.getElementById('heading')
 console.log(heading2);
+
+//generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+//agregar el href
+nuevoEnlace.href ='nuevo-enlace.html';
+//agregar el texto
+nuevoEnlace.textContent = 'Un nuevo enlace';
+
+//agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+console.log(nuevoEnlace)
