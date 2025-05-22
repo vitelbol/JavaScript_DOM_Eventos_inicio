27927b623e905279d67027c65bb542e5014eeacf
jose villegas <josevillegas13600@gmail.com>
2025-05-21 22:54:10 -0400

    queryselector

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// querySelector
+const heading =document.querySelector('.header__texto h2') // 0 o 1 elementos
+console.log(heading);
+heading.textContent = 'Nuevo heading';
+//queryselectorall
+
+//getelementbyid
