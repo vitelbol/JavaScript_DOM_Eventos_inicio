fd1f0df56416aeef88fbffa28966d1440b868bcf
jose villegas <josevillegas13600@gmail.com>
2025-05-21 23:03:40 -0400

    queryselectorall

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,11 @@ const heading =document.querySelector('.header__texto h2') // 0 o 1 elementos
 console.log(heading);
 heading.textContent = 'Nuevo heading';
 //queryselectorall
-
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
+enlaces[0].textContent = 'Nuevo texto para enlace';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 //getelementbyid
+const heading2 = document.getElementById('heading')
+console.log(heading2);
